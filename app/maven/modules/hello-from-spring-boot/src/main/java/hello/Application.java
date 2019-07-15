d3dee82a3e85bfe3223a74bc2b9b9c9901498372
Bruno Alves <brunosalves3@gmail.com>
2019-07-15 02:21:49 -0300

    wip

--- app/maven/modules/hello-from-spring-boot/src/main/java/hello/Application.java
+++ app/maven/modules/hello-from-spring-boot/src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "Hello from Spring Boot!";
     }
 
     public static void main(String[] args) {
